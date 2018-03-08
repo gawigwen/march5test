7aeee7b38274f72fcc0a592e8cd013c10a283b54
Dylan Tucker <dylantt2000@gmail.com>
2018-03-07 14:01:42 -1000

    push

--- app.js
+++ app.js
@@ -5,10 +5,18 @@ var classes=[
 "English"
 ];
 
-var x=10;
+var x= "Coding";
 classes[3] = x;
 
-function loop(){
-
+for (i = 0; i < classes.length; i++) { 
+    text += cars[i] + "<br>";
 }
-
+getElementByID('')
+function clicked(){
+	var y = "It's a lie";
+	document.getElementById("idkwattodo").innerHTML = y;
+};
+function ec(){
+var bonus= classes[Math.floor((Math.random() * 3) + 1)];
+document.getElementById("fourth").innerHTML = bonus;
+}
